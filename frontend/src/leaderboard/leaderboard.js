f4b19e09f18e832e11a4acb342649ceb4e371871
Frost Tianjian Xu <xutianjian@gmail.com>
2020-10-25 13:51:39 -0700

    Creating new leaderboard page

--- frontend/src/leaderboard/leaderboard.js
+++ frontend/src/leaderboard/leaderboard.js
@@ -1,224 +1,51 @@
 import React, { Component } from "react";
-import { List, Avatar, Row, Col } from 'antd';
+import Papa from "papaparse";
+import Leadergraph from "./leadergraph";
 import "../covid19app.css";
-import "./leaderboard.css";
-import nyt_graph from "./img/nyt_graph.png"
-import jhu_graph from "./img/jhu_graph.png"
-import usf_graph from "./img/usf_graph.png"
-import all_graph from "./img/all_graph.png"
-
-const data = {
-    jhu: {
-runningAvgRankings: [
-    {
-     model: {
- name: "YYG_ParamSearch",
- description: "Based on the SEIR model to make daily projections regarding COVID-19 infections and deaths in 50 US states. The model's contributor is Youyang Gu.",
- link: "http://covid19-projections.com/about/"
-},
- RMSE: 34.35
-},  {
-     model: {
- name: "SIkJa_USC",
- description: "This is our SI-kJalpha model.",
- link: "https://scc-usc.github.io/ReCOVER-COVID-19/"
-},
- RMSE: 35.41
-},  {
-     model: {
- name: "UCLA_SuEIR",
- description: "SEIR model by UCLA Statistical Machine Learning Lab.",
- link: "https://covid19.uclaml.org/"
-},
- RMSE: 52.53
-},  {
-     model: {
- name: "Covid19Sim_Simulator",
- description: "An interactive tool developed by researchers at Mass General Hospital, Harvard Medical School, Georgia Tech and Boston Medical Center.",
- link: "https://covid19sim.org/"
-},
- RMSE: 58.28
-},  {
-     model: {
- name: "CU_select",
- description: "A metapopulation county-level SEIR model by Columbia University.",
- link: "https://blogs.cuit.columbia.edu/jls106/publications/covid-19-findings-simulations/"
-},
- RMSE: 64.22
-},  {
-     model: {
- name: "JHU_IDD_CovidSP",
- description: "County-level metapopulation model by Johns Hopkins ID Dynamics COVID-19 Working Group.",
- link: "https://github.com/HopkinsIDD/COVIDScenarioPipeline"
-},
- RMSE: 72.68
-},  {
-     model: {
- name: "IowaStateLW_STEM",
- description: "A COVID19 forecast project led by Lily Wang in Iowa State University.",
- link: "https://covid19.stat.iastate.edu"
-},
- RMSE: 76.08
-},  {
-     model: {
- name: "CovidActNow_SEIR_CAN",
- description: "SEIR model by the CovidActNow research team.",
- link: "https://covidactnow.org/"
-},
- RMSE: 110.82
-},],
-recentRankings: [
-    {
-     model: {
- name: "YYG_ParamSearch",
- description: "Based on the SEIR model to make daily projections regarding COVID-19 infections and deaths in 50 US states. The model's contributor is Youyang Gu.",
- link: "http://covid19-projections.com/about/"
-},
- RMSE: 18.6
-},  {
-     model: {
- name: "SIkJa_USC",
- description: "This is our SI-kJalpha model.",
- link: "https://scc-usc.github.io/ReCOVER-COVID-19/"
-},
- RMSE: 20.03
-},  {
-     model: {
- name: "Covid19Sim_Simulator",
- description: "An interactive tool developed by researchers at Mass General Hospital, Harvard Medical School, Georgia Tech and Boston Medical Center.",
- link: "https://covid19sim.org/"
-},
- RMSE: 20.58
-},  {
-     model: {
- name: "JHU_IDD_CovidSP",
- description: "County-level metapopulation model by Johns Hopkins ID Dynamics COVID-19 Working Group.",
- link: "https://github.com/HopkinsIDD/COVIDScenarioPipeline"
-},
- RMSE: 24.22
-},  {
-     model: {
- name: "UCLA_SuEIR",
- description: "SEIR model by UCLA Statistical Machine Learning Lab.",
- link: "https://covid19.uclaml.org/"
-},
- RMSE: 24.6
-},  {
-     model: {
- name: "CU_select",
- description: "A metapopulation county-level SEIR model by Columbia University.",
- link: "https://blogs.cuit.columbia.edu/jls106/publications/covid-19-findings-simulations/"
-},
- RMSE: 40.24
-},  {
-     model: {
- name: "IowaStateLW_STEM",
- description: "A COVID19 forecast project led by Lily Wang in Iowa State University.",
- link: "https://covid19.stat.iastate.edu"
-},
- RMSE: 40.41
-},  {
-     model: {
- name: "CovidActNow_SEIR_CAN",
- description: "SEIR model by the CovidActNow research team.",
- link: "https://covidactnow.org/"
-},
- RMSE: NaN
-},]
-    }
-};
-
+// import "./leaderboard.css";
+import summaryCSV from "./summary/summary_4_weeks_ahead_states.csv";
 class Leaderboard extends Component {
-    getAvatar(number) {
-        let icon_src = "";
-        switch (number) {
-            case 1:
-                icon_src = "https://img.icons8.com/officel/80/000000/medal2.png";
-                break;
-            case 2:
-                icon_src = "https://img.icons8.com/officel/80/000000/medal-second-place.png";
-                break;
-            case 3:
-                icon_src = "https://img.icons8.com/officel/80/000000/medal2-third-place.png";
-                break;
-            default:
-                icon_src = "https://img.icons8.com/carbon-copy/100/000000/" + number + "-circle.png";
-                break;
-        }
 
-        return <Avatar className="rank-number" src={icon_src} alt="" />;
+    constructor(props) {
+        super(props);
+        this.state = {summary: []};
+    }
 
-    };
+    componentDidMount = () => {
+        Papa.parse(summaryCSV, {
+            header: true,
+            download: true,
+            skipEmptyLines: true,
+            complete: this.updateData
+        })
+    }
+
+    updateData = (result) => {
+        console.log(result.data);
+        const modelsSummary = result.data.map(csvRow => {
+            const model = {id: "", data: []};
+            for (const col in csvRow) {
+                if (col === "") {
+                    model.id = csvRow[col];
+                } else if (col.indexOf("mean_sq_abs_error_") >= 0 
+                && csvRow[col] != null 
+                && csvRow[col] != "") {
+                    model.data.push({
+                        "x": col.substring(18, col.length - 1),
+                        "y": parseInt(csvRow[col])
+                    });
+                }
+            }
+            return model;
+        });
+        console.log(modelsSummary);
+        this.setState({summary: modelsSummary.splice(0, 5)});
+    }
 
     render() {
-        return (
-            <div className="page-wrapper">
-                <div className="grid">
-                    <Row>
-                        
-                        <Col span={12}>
-                            <h2 className="title">Running Average Performance</h2>
-                            <List className="leaderboard"
-                                itemLayout="horizontal"
-                                dataSource={data.jhu.runningAvgRankings}
-                                renderItem={item => (
-                                    <List.Item>
-                                        <List.Item.Meta
-                                            avatar={this.getAvatar(data.jhu.runningAvgRankings.indexOf(item) + 1)}
-                                            title={<a className="model-name" href={item.model.link}>{item.model.name}</a>}
-                                            description={item.model.description}
-                                        />
-                                        <div className="content">
-                                            <span>RMSE: <span className="score">{item.RMSE}</span></span>
-                                        </div>
-                                    </List.Item>
-                                )}
-                            />
-                        </Col>
-                        <Col span={12}>
-                            <h2 className="title">Recent Performance (from 2020-09-20)</h2>
-                            <List className="leaderboard"
-                                itemLayout="horizontal"
-                                dataSource={data.jhu.recentRankings}
-                                renderItem={item => (
-                                    <List.Item>
-                                        <List.Item.Meta
-                                            avatar={this.getAvatar(data.jhu.recentRankings.indexOf(item) + 1)}
-                                            title={<a className="model-name" href={item.model.link}>{item.model.name}</a>}
-                                            description={item.model.description}
-                                        />
-                                        <div className="content">
-                                            <span>RMSE: <span className="score">{item.RMSE}</span></span>
-                                        </div>
-                                    </List.Item>
-                                )}
-                            />
-                        </Col>
-                    </Row> 
-
-                    <Row>
-                        <div className="main-graph-container">
-                            <img className="graph" src={all_graph} />
-                        </div>
-                        
-                    </Row>
-
-		            <p className="clarification">
-                        <b>Evaluation method:</b> We have recently moved the evaluation to JHU dataset only as majority of the forecasting teams are now using it. <br />
-                        Our forecasts used here are taken from the our <a href='https://github.com/scc-usc/ReCOVER-COVID-19/tree/master/results/forecasts'> github repo </a>.
-                        The chosen evaluation metric is Root Mean Squared Error (RMSE) of weekly new deaths computed <br />
-                        over the next two weeks from the day of the forecasts. <br />
-                        Please contact us at <a className="article-anchor" href="mailto:ajiteshs@usc.edu">ajiteshs@usc.edu</a>  
-                        to add your model to the leaderboard.
-                    </p>
-                    <p className="disclaimer">
-                        <b>Disclaimer:</b> The above Covid-19 forecast reports may have copyright restrictions. 
-                        You may visit the website of their original work by clicking on their model names. <br />
-                        All credits for predictions go to the respective owners of the forecast reports.
-                        The ReCover-Covid-19 website does not hold responsibility or liability for prediction accuracy
-                        of prediction reports that are not generated by USC Data Science lab.
-                    </p> 
-                </div>
+        return(
+            <div className="graph-container">
+                <p>Gello?</p>
             </div>
         );
     }
---
+++ frontend/src/leaderboard/leadergraph.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from "react";
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Brush,
+  AreaChart, Area,
+} from 'recharts';
+
+class Leadergraph extends PureComponent {
+}
+
+export default Leadergraph;
